142cdbb8d94d37541c4d5e4e02edfb1774dfd056
Ayo Abimbola <ayoinspired@gmail.com>
2021-09-16 13:57:25 +0100

    Added calendar on Header

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+
 import Image from "next/image";
 import {
 	SearchIcon,
@@ -8,6 +13,21 @@ import {
 } from "@heroicons/react/solid";
 
 function Header() {
+	const [searchInput, setSearchInput] = useState("");
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: "selection",
+	};
+
+	const handleSelect = (ranges) => {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	};
+
 	return (
 		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-3 px-4 md:px-10">
 			<div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -21,6 +41,8 @@ function Header() {
 
 			<div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
 				<input
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value)}
 					className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
 					type="text"
 					placeholder="Start Your Search"
@@ -37,6 +59,29 @@ function Header() {
 					<UserCircleIcon className="h-6 cursor-pointer" />
 				</div>
 			</div>
+
+			{searchInput && (
+				<div className="flex flex-col col-span-3 mx-auto">
+					<DateRangePicker
+						ranges={[selectionRange]}
+						minDate={new Date()}
+						rangeColors={["#FD5B61"]}
+						onChange={handleSelect}
+					/>
+
+					<div className="flex items-center border-b mb-4">
+						<h2 className="text-2xl flex-grow font-semibold">
+							Number of Guests
+						</h2>
+
+						<UsersIcon className="h-5" />
+						<input
+							type="number"
+							className="w-12 pl-2 text-lg outline-none text-red-400"
+						/>
+					</div>
+				</div>
+			)}
 		</header>
 	);
 }
